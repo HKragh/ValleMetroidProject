c0476bd44c49db93300632327721f9c70aabbdb5
Marcel <Marcel@kress.dk>
2022-11-21 14:28:47 +0100

    Health system

--- ValleMetroid/Assets/Scripts/Health/P1/Health.cs
+++ ValleMetroid/Assets/Scripts/Health/P1/Health.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
-    private float currentHealth;
-
+    public float currentHealth { get; private set; }
+    public KeyCode damageKey;
 
     private void Awake()
     {
         currentHealth = startingHealth;
     }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if(currentHealth > 0)
+        {
+            //take damage
+        }
+        else
+        {
+            //Player death
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(damageKey))
+        {
+            TakeDamage(1);
+        }
+    }
+
+
+
 }
