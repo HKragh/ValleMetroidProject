7c634a732213b5df64092f088cf3810508ef6a1f
Markus <markusdk10@gmail.com>
2022-11-28 13:33:24 +0100

    Cam update

--- ValleMetroid/Assets/Scripts/CameraTarget.cs
+++ ValleMetroid/Assets/Scripts/CameraTarget.cs
@@ -8,10 +8,24 @@ public class CameraTarget : MonoBehaviour
     public Transform target2;
     public Vector3 offset;
 
+    private float yValue;
+
+    public void Start()
+    {
+        yValue = transform.position.y;
+
+    }
+
     private void LateUpdate()
     {
         transform.position = ((target1.position + target2.position) / 2f) + offset;
 
+        Vector3 currentPossition = transform.position;
+        currentPossition.y = yValue;
+        transform.position = currentPossition;
+
+
+        
     }
    
 
